c6f313e2067a6594a6035f6da9085e132d374f02
CrisleyWolgien <Crisleyws@gmail.com>
2025-04-05 18:22:54 -0300

    Finalizado

--- Jogo_de_perguntas.py
+++ Jogo_de_perguntas.py
@@ -1,13 +1,24 @@
 import json
 import random
 import time
+import sys
+
+
+def recarregar_json():
+    with open("perguntas.json", encoding="utf-8") as file:
+        perguntas = json.load(file)  # abre o json
+    return perguntas
+
 
 with open("perguntas.json", encoding="utf-8") as file:
     perguntas = json.load(file)  # abre o json
 
 
-id_perguntas = {int(k): v for k, v in perguntas.items()}  # tranforma o id em int
+id_perguntas = {int(k): v for k, v in perguntas.items()}# tranforma o id em in
 perguntas_tamanho_dict = len(perguntas)  # define o tamanho de dicionario
+print(id_perguntas.keys())  # imprime os ids das perguntas
+id_perguntas_lista = list(id_perguntas.keys())  # transforma o id em lista
+
 
 # inicializacao de dados
 respostas_corretas = 0
@@ -16,7 +27,7 @@ rodadas_jogadas = 0
 
 
 def randomiza_pergunta():
-    random_da_pergunta = random.randrange(1, perguntas_tamanho_dict + 1)  # define o range do random conforme o tamanho do dicionário
+    random_da_pergunta = random.choice(id_perguntas_lista)  # define o range do random conforme o tamanho do dicionário
     return random_da_pergunta
 
 
@@ -101,6 +112,9 @@ def adiciona_pergunta():
 
     with open("perguntas.json", "w", encoding="utf-8") as file:
         json.dump(perguntas, file, indent=4, ensure_ascii=False)
+    recarregar_json()
+    print("pergunta adicionada com sucesso")
+    time.sleep(1)
 
 
 # EXCLUIR PERGUNTA
@@ -112,13 +126,24 @@ def excluir_pergunta():
         nome_pergunta, opcao_1, opcao_2, opcao_3, opcao_4, resposta_pergunta = pergunta(pergunta_selecionada)
         print()
         print(pergunta_id + " - " + nome_pergunta)
+    pergunta_selecionada_excluir = input(f"Digite o id da pergunta que deseja excluir: ")
+    while pergunta_selecionada_excluir not in perguntas.keys():
+        print("pergunta não encontrada")
+        pergunta_selecionada_excluir = input(f"Digite o id da pergunta que deseja excluir: ")
+    del perguntas[pergunta_selecionada_excluir]
+    with open("perguntas.json", "w", encoding="utf-8") as file:
+        json.dump(perguntas, file, indent=4, ensure_ascii=False)
+    recarregar_json()
+    print("pergunta excluida com sucesso")
     time.sleep(1)
+
 
 # menu do programa
 sair_progrma = False
 
 
 def menu(rodadas_jogadas, respostas_corretas, respostas_erradas, sair_progrma):
+    recarregar_json()
     print()
     print("Bem vindo ao Jogo de perguntas")
     print("Escolha uma opção")
@@ -140,7 +165,6 @@ def menu(rodadas_jogadas, respostas_corretas, respostas_erradas, sair_progrma):
             print("quantas rodadas vai querer")
             print(f"de 1 a {perguntas_tamanho_dict}")
             rodadas_selecionada = input()
-            0
             while int(rodadas_selecionada) > perguntas_tamanho_dict:
                 print("você escolheu um numero incompatível")
                 print(f"escolha um numero de 1 a {perguntas_tamanho_dict}")
@@ -155,16 +179,16 @@ def menu(rodadas_jogadas, respostas_corretas, respostas_erradas, sair_progrma):
             print(f"Você jogou um total de {rodadas_jogadas} rodadas")
             print(f"Você acertou um total de {respostas_corretas} respostas")
             print(f"Você errou um total de {respostas_erradas} respostas")
+            time.sleep(1)
+            print()
 
         case 2:
             adiciona_pergunta()
         case 3:
             excluir_pergunta()
         case 0:
-            sair_progrma = True
             print("vc saiu")
-            return sair_progrma
-
+            sys.exit()
 
 while not sair_progrma:
     menu(rodadas_jogadas, respostas_corretas, respostas_erradas, sair_progrma)
